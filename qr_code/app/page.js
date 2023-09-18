0571b07ac85b1f9f7c5f1074b87b04058ec1a915
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-09-19 01:20:43 +0530

    complete qr code mini project

--- qr_code/app/page.js
+++ qr_code/app/page.js
@@ -1,9 +1,23 @@
-
+"use client"
+import QRCode from 'react-qr-code'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [text, setText] = useState('')
+  function handleChange(e) {
+    setText(e.target.value)
+  }
   return (
     <div>
-      Hi
+      <h1>QR Code</h1>
+      <QRCode value={text} /> 
+
+      <div>
+        <p>Enter your text</p>
+        <input type="text" value={ text} onChange={(e)=>{handleChange(e)}} />
+        <button>Generate</button>
+      </div>
     </div>
   )
 }
